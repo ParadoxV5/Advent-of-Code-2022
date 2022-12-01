3ebdd17cf8ef8c2042fdf2ce21931394b1f427b2
ParadoxV5 <paradox.ver5@gmail.com>
2022-12-01 00:19:48 -0700

    Day 1, alternate solution
    uses `slice_after` to group numbers instead of dividing text first by paragraph and then by line

--- 1 Calorie Counting.rb
+++ 1 Calorie Counting.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
-# Part 1: …}.max
-puts File.foreach('input.txt', '').map { _1.lines.sum(&:to_i) }.max(3).sum
+puts File.
+  foreach('input.txt'). # each line
+  slice_after(/^$/). # group using blank line as dividers
+  map { _1.sum(&:to_i) }. # map to sum as integers
+# Part 1:
+#.max
+# Part 2:
+max(3).sum
