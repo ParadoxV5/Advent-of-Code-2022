204cfcf7cf5aad3ddb3c4c36cf4c9dabc6d4b488
ParadoxV5 <paradox.ver5@gmail.com>
2022-12-01 23:07:52 -0700

    Day 2, combined both parts into one
    half of each were common code

--- 2 Rock Paper Scissors.rb
+++ 2 Rock Paper Scissors.rb
@@ -1,36 +1,24 @@
 # frozen_string_literal: true
 
-puts 'Part 1'
+matches = File.readlines('input.txt', chomp: true) # each line without the line break
 
 # Actually easier and less brainpower to just hardcode the combinations out
-scores = [nil,
-  # You chose:
-  # Rck,   Ppr,   Ssr
-  #  1 ,    2 ,    3
-  'B X', 'C Y', 'A Z', # 0
-  'A X', 'B Y', 'C Z', # 3
-  'C X', 'A Y', 'B Z'  # 6
-].each_with_index.to_h # match => score
-
-puts File.
-  foreach('input.txt', chomp: true). # each line without the line break
-  map { scores[_1] }. # map to possibility, index = the score
-sum
-
-
-puts 'Part 2'
-
-# Actually easier and less brainpower to just hardcode the combinations out
-scores = [nil,
-  # You chose:
-  # Rck,   Ppr,   Ssr
-  #  1 ,    2 ,    3
-  'B X', 'C X', 'A X', # 0
-  'A Y', 'B Y', 'C Y', # 3
-  'C Z', 'A Z', 'B Z'  # 6
-].each_with_index.to_h # match => score
-
-puts File.
-  foreach('input.txt', chomp: true). # each line without the line break
-  map { scores[_1] }. # map to possibility, index = the score
-sum
+# You chose:
+#    Rock, Paper, Scissors
+#      1 ,    2 ,    3
+puts(
+  [  [nil, # Part 1 key
+    'B X', 'C Y', 'A Z', # 0
+    'A X', 'B Y', 'C Z', # 3
+    'C X', 'A Y', 'B Z'  # 6
+  ], [nil, # Part 2 key
+    'B X', 'C X', 'A X', # 0
+    'A Y', 'B Y', 'C Y', # 3
+    'C Z', 'A Z', 'B Z'  # 6
+  ]  ].map!.with_index do |key, i|
+    puts "Part #{i.succ}"
+    # Convert to `match => score` hash for fast indexing
+    key = key.each_with_index.to_h
+    matches.sum { key[_1] } # sum as score
+  end
+)
