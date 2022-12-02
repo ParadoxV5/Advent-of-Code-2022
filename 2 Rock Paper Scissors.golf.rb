e1c30c05bfe5a299d607746b57d9faeae003c435
ParadoxV5 <paradox.ver5@gmail.com>
2022-12-02 03:27:37 -0700

    Day 2, ⛳, separate parts

--- 2 Rock Paper Scissors.golf.rb
+++ 2 Rock Paper Scissors.golf.rb
@@ -1,2 +1,7 @@
 # Both parts, directly converted from the ordinary solution
 #i=*$<;['XYZ'*3,'XXXYYYZZZ'].each{|k|p i.sum{'BCAABCCAB'.chars.zip([' ']*9,k.chars).index(_1.chop.chars)+1}}
+
+# Part 1
+#p$<.sum{a,x=_1.unpack 'CxC';(x+~a)%3*3+x-87}
+# Part 2
+#p$<.sum{a,x=_1.unpack 'CxC';x-=1;(a+x)%3+x%3*3+1}
