fe8dd8fd591e08cf90601406926286e229dc5f4a
ParadoxV5 <paradox.ver5@gmail.com>
2022-12-01 22:43:54 -0700

    Day 2 Part 2

--- 2 Rock Paper Scissors.rb
+++ 2 Rock Paper Scissors.rb
@@ -16,3 +16,21 @@ puts File.
   foreach('input.txt', chomp: true). # each line without the line break
   map { scores[_1] }. # map to possibility, index = the score
 sum
+
+
+puts 'Part 2'
+
+# Actually easier and less brainpower to just hardcode the combinations out
+scores = [nil,
+  # You chose:
+  # Rck,   Ppr,   Ssr
+  #  1 ,    2 ,    3
+  'B X', 'C X', 'A X', # 0
+  'A Y', 'B Y', 'C Y', # 3
+  'C Z', 'A Z', 'B Z'  # 6
+].each_with_index.to_h # match => score
+
+puts File.
+  foreach('input.txt', chomp: true). # each line without the line break
+  map { scores[_1] }. # map to possibility, index = the score
+sum
