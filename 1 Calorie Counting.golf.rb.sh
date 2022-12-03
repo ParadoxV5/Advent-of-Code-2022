827011cd7c92ae2121b612cb5aa87bdc863ed09a
ParadoxV5 <paradox.ver5@gmail.com>
2022-12-03 00:47:41 -0700

    Reformatted ⛳ boilerplate for previous 2 days

--- 1 Calorie Counting.golf.rb.sh
+++ 1 Calorie Counting.golf.rb.sh
@@ -1,2 +1,2 @@
-# 54 chars
+# Both Parts: 54 chars
 cat input.txt | ruby -e '$/="";m=$<.map{_1.split.sum &:to_i}.max 3;p m[0],m.sum'
--- 2 Rock Paper Scissors.golf.rb.sh
+++ 2 Rock Paper Scissors.golf.rb.sh
@@ -1,10 +1,10 @@
-# Both parts, directly converted from the ordinary solution
+# Both Parts, directly converted from the ordinary solution
 #i=*$<;['XYZ'*3,'XXXYYYZZZ'].each{|k|p i.sum{'BCAABCCAB'.chars.zip([' ']*9,k.chars).index(_1.chop.chars)+1}}
 #
-# Part 1
+# Part 1: 39 chars
 #p$<.sum{a,_,x=_1.bytes;(x+~a)%3*3+x-87}
-# Part 2
+# Part 2: 44 chars
 #p$<.sum{a,_,x=_1.bytes;x-=1;(a+x)%3+x%3*3+1}
 #
-# 76 chars
+# Both Parts: 76 chars
 cat input.txt | ruby -e 'u=v=0;$<.map{a,_,x=_1.bytes;x-=1;u+=(x-a)%3*3+x-86;v+=(x+a)%3+x%3*3+1};p u,v'
