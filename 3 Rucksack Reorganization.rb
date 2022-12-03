eade00724580b8355cfa6a41808d3f91aaafadd1
ParadoxV5 <paradox.ver5@gmail.com>
2022-12-02 22:50:18 -0700

    Day 3, transition: move innermost block to method
    both parts have this step

--- 3 Rucksack Reorganization.rb
+++ 3 Rucksack Reorganization.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 require 'set'
 
+class Set
+  def aoc_priority
+    o = first.ord
+    # 'a'.ord => 97; 'A'.ord => 65;  27-65 => -38
+    o > 90 ? o - 96 : o - 38
+  end
+end
+
 puts('Part 1',
   File.foreach('input.txt', chomp: true).sum do |line|
     half_length = line.size / 2
-    (line[...half_length].each_char.to_set & line[half_length..].each_char).sum do |x|
-      x = x.ord
-      # 'a'.ord => 97; 'A'.ord => 65;  27-65 => -38
-      x > 90 ? x - 96 : x - 38
-    end
+    (line[...half_length].each_char.to_set & line[half_length..].each_char).aoc_priority
   end
 )
