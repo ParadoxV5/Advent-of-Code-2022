1760b8ee1063e112ef23da1634b2b407e3ce4c6c
ParadoxV5 <paradox.ver5@gmail.com>
2022-12-03 01:20:39 -0700

    Day 3 ⛳ part 2

--- 3 Rucksack Reorganization.golf.rb.sh
+++ 3 Rucksack Reorganization.golf.rb.sh
@@ -1,2 +1,7 @@
 # Part 1: 71 chars
 #p $<.sum{((_1.slice!(0,_1.size/2).bytes.uniq&_1.bytes.uniq).sum 20)%58}
+# Part 2: 70 chars
+#p $<.each_slice(3).sum{|g|(g.map{_1.bytes.uniq}.inject(:&).sum 10)%58}
+#
+# Both Parts: ? chars
+cat input.txt | ruby -e ''
