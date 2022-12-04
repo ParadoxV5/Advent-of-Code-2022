3b118eeb0758fed52360d659397bf5763ba5eaf5
ParadoxV5 <paradox.ver5@gmail.com>
2022-12-03 22:36:53 -0700

    Day 4, replace part 1 to match part 2 in strat
    replace Ruby method with manual conditions

--- 4 Camp Cleanup.rb
+++ 4 Camp Cleanup.rb
@@ -4,9 +4,8 @@ puts(
   File.foreach('input.txt').count do |pair|
     # "a-b,x-y"
     a,b , x,y = pair.scan(/\d++/).map!(&:to_i)
-    # Part 1
-    #a, x = a..b, x..y
-    #a.cover?(x) or x.cover?(a)
+    # Part 1: a..b contains x..y if a<=x and y<=b, vice versa
+    #(a <= x and y <= b) or (x <= a and b <= y)
     # Part 2: Does not overlap if b<x or y<a
     b >= x and y >= a
   end
