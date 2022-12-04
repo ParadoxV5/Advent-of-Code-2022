2babe2081f1d90837e066a26b9591cb36d652f2a
ParadoxV5 <paradox.ver5@gmail.com>
2022-12-03 22:30:50 -0700

    Day 4 part 2

--- 4 Camp Cleanup.rb
+++ 4 Camp Cleanup.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 puts(
-  'Part 1',
   File.foreach('input.txt').count do |pair|
     # "a-b,x-y"
     a,b , x,y = pair.scan(/\d++/).map!(&:to_i)
-    a, x = a..b, x..y
-    a.cover?(x) or x.cover?(a)
+    # Part 1
+    #a, x = a..b, x..y
+    #a.cover?(x) or x.cover?(a)
+    # Part 2: Does not overlap if b<x or y<a
+    b >= x and y >= a
   end
 )
