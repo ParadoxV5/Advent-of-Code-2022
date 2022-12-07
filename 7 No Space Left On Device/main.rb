fcc153dd74f9067548c91df8d36d4f8914a83236
ParadoxV5 <paradox.ver5@gmail.com>
2022-12-07 00:44:35 -0700

    Day 7
    difficulty spike today with the involvement of a Tree data structure

---
+++ 7 No Space Left On Device/main.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+class Directory < Hash
+  attr_reader :parent
+  attr_reader :dir_size
+  def initialize(parent)
+    @parent = parent
+    @dir_size = 0
+  end
+  
+  def add_file(amount)
+    @dir_size += amount
+    parent&.add_file(amount)
+  end
+  def dir_sizes
+    each_value.flat_map{ _1.dir_sizes } << dir_size
+  end
+end
+
+cd = ROOT = Directory.new(nil)
+File.foreach('input.txt', chomp: true) do |line|
+  next if line.eql?('$ cd /')
+  detail, _, name = line.rpartition(' ')
+  case detail
+  when '$ cd'
+    cd = name.eql?('..') ? cd.parent : cd[name]
+  when 'dir'
+    cd[name] = Directory.new(cd)
+  else
+    cd.add_file(detail.to_i)
+  end
+end
+
+dir_sizes = ROOT.dir_sizes
+required_space = dir_sizes.last - 40000000 # for Part 2
+
+puts(
+  'Part 1',
+  dir_sizes.lazy.filter { _1 <= 100000 }.sum,
+  'Part 2',
+  dir_sizes.lazy.filter { _1 >= required_space }.min
+)
