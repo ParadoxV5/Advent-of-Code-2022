0c13589b64f78ff41238d4ac01aaa5bd4d472456
ParadoxV5 <paradox.ver5@gmail.com>
2022-12-07 21:29:35 -0700

    Day 7 ⛳ tiny revert with fix

--- 7 No Space Left On Device/golf.rb
+++ 7 No Space Left On Device/golf.rb
@@ -1,5 +1,5 @@
 # Both Parts: 119 chars
 A=[]
-P=->n=0{$<.map{|l|l['..']&&break;n+=l['d ']?P[]:l.to_i};A<<n;n}
+P=->n=0{$<.map{|l|l['..']?break: n+=l['d ']?P[]:l.to_i};A<<n;n}
 R=P[]-4e7
 p A.sum{_1<=1e5?_1:0},A.filter{_1>=R}.min
