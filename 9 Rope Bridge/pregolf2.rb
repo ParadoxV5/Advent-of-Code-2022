51ae4115e5770d990dab7ea2472a81400e38641d
ParadoxV5 <paradox.ver5@gmail.com>
2022-12-09 15:55:08 -0700

    Update Day 9 Part 2 pre⛳ to match ⛳

---
+++ 9 Rope Bridge/pregolf2.rb
@@ -0,0 +1,22 @@
+N = 10 # Part 1: `2`
+KNOTS = Array.new(N, 0i) # Complex suggested by Discord@sampersand#8419
+TAIL_POSITIONS = []
+DIRECTION_UNITS = 'RULD'.each_char.with_index.to_h { [_1, 1i ** _2] }
+
+while (direction = $stdin.getc)
+  $stdin.gets.to_i.times do
+    # Move head
+    KNOTS[0] += DIRECTION_UNITS[direction]
+    # Update each following knot
+    N.times.each_cons(2) do |prev_index, succ_index|
+      delta = KNOTS[prev_index] - KNOTS[succ_index]
+      if delta.abs >= 2
+        KNOTS[succ_index] += Complex.rect(*delta.rect.map! { _1 <=> 0 })
+      end
+    end
+    # Record tail position
+    TAIL_POSITIONS << KNOTS.last
+  end
+end
+
+puts TAIL_POSITIONS.uniq.size
