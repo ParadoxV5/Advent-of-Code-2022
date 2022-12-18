4335c8182942b504eb0ab4a514575d72ee72445a
ParadoxV5 <paradox.ver5@gmail.com>
2022-12-17 23:52:41 -0700

    Day 18 Part 2

--- 18 Boiling Boulders/main.rb
+++ 18 Boiling Boulders/main.rb
@@ -1,11 +1,38 @@
 require 'set'
-CUBES = File.foreach('input.txt').map { _1.split(',').map(&:to_i) }.to_set
 
-puts CUBES.flat_map {|x, y, z| [
-  [x.pred, y, z],
-  [x.succ, y, z],
-  [x, y.pred, z],
-  [x, y.succ, z],
-  [x, y, z.pred],
-  [x, y, z.succ]
-] }.count {|surface| not CUBES.include?(surface) }
+cubes = File.foreach('input.txt').map { _1.split(',').map(&:to_i) }
+cubes_set = cubes.to_set
+
+def neighbors(c)
+  x, y, z = *c
+  [
+    [x.pred, y, z],
+    [x.succ, y, z],
+    [x, y.pred, z],
+    [x, y.succ, z],
+    [x, y, z.pred],
+    [x, y, z.succ]
+  ]
+end
+
+surfaces = cubes.flat_map { neighbors(_1) }.reject { cubes_set.include?(_1) }
+
+puts 'Part 1', surfaces.size
+
+# Strategy for Part 2:
+# identify all air that is reachable from a cube of air that is known to be non-trapped
+# All unreachable air are pockets trapped off from outside
+# (Using BFS again because DFS costs too much memory and raised SystemStackError 😓)
+
+dim_min, dim_max = cubes.flatten.minmax
+air_to_search = Range.new(dim_min.pred, dim_max.succ).to_a.repeated_permutation(3).reject { cubes_set.include?(_1) }.to_set
+surfaces_to_filter = surfaces.to_set
+
+bfs = [ Array.new(3, dim_min) ]
+while (this = bfs.pop)
+  # Delete from listings, then queue up neighbors if valid
+  surfaces_to_filter.delete(this)
+  bfs.push(*neighbors(this)) if air_to_search.delete?(this)
+end
+
+puts 'Part 2', surfaces.count { not surfaces_to_filter.include?(_1) }
