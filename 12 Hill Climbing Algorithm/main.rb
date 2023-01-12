1a28f1d0f372fc75f05b24f17ea1651e774186fa
ParadoxV5 <paradox.ver5@gmail.com>
2023-01-11 17:24:06 -0700

    Update Day 12

--- 12 Hill Climbing Algorithm/main.rb
+++ 12 Hill Climbing Algorithm/main.rb
@@ -1,8 +1,7 @@
-$bfs = []
 MAP = File.foreach('input.txt', chomp: true).map.with_index do |row, y|
   row = row.chars
   if (x = row.find_index('E'))
-    $bfs << [y, x]
+    $bfs = [ [y, x] ]
     row[x] = 'z'
   end
   row
@@ -18,23 +17,34 @@ loop do
     here = MAP[y0][x0]
     next unless here
     
-    [
-      [y0.pred, x0],
-      [y0.succ, x0],
-      [y0, x0.pred],
-      [y0, x0.succ]
-    ].each do |there_pos|
-      next if there_pos.any?(:negative?)
-      there = MAP.dig(*there_pos)
-      next unless there
-      if there.ord >= here.ord.pred
-        if there <= 'a'
-          puts steps
-          exit
+    $bfs2.push(*
+      [
+        [y0.pred, x0],
+        [y0.succ, x0],
+        [y0, x0.pred],
+        [y0, x0.succ]
+      ].select do |there_pos|
+        next if there_pos.any?(:negative?)
+        there = MAP.dig(*there_pos)
+        next unless there
+        
+        # Part 1:
+        #if there < 'a'
+        #  puts steps
+        #  exit
+        #end
+        #there.ord >= here.ord.pred
+        
+        # Part 2:
+        if there.ord >= here.ord.pred
+          if there <= 'a'
+            puts steps
+            exit
+          end
+          true
         end
-        $bfs2 << there_pos
       end
-    end
+    )
     MAP[y0][x0] = nil # remove locations that have been searched
   end
   $bfs = $bfs2
