e080fb50a31a691a6b0aab84d68a1198cb98a37e
ParadoxV5 <paradox.ver5@gmail.com>
2023-01-19 02:53:34 -0700

    Day 22 Part 1
    Did much more and took much longer than one would expect. Hah! Might wanna fix that one, especially now that I’m not racing for the leaderboard.

---
+++ 22 Monkey Map/main1.rb
@@ -0,0 +1,110 @@
+# Board comprised of a single orthogonally convex shape
+# Compile to indices of board edges and walls
+# Local Wrap Around
+class ConvexBoard
+  class LandSequence
+    # `@range` must not `#exclude_end?`
+    attr_reader :range, :obstructions
+    def initialize(range, obstructions)
+      @range, @obstructions = range, obstructions
+    end
+    def self.[](*array, is_land: :itself, is_obstruction:)
+      # Index of first land – Index of last land
+      range = array.index(&is_land)..array.rindex(&is_land)
+       # Range above; Indices of obstructions within the range above
+      ConvexBoard::LandSequence.new(
+        range,
+        range.select { array[_1].then(&is_obstruction) }
+      )
+    end
+    
+    def step(from, to = from.succ)
+      if to >= from
+        path = from..to
+        obstructions.find { path.cover?(_1) }&.pred
+      else
+        path = to..from
+        obstructions.reverse_each.find { path.cover?(_1) }&.succ
+      end or to
+    end
+    def step_by(from, steps = 1)
+      to = from + steps
+      if range.cover?(to) # No wrap around
+        step(from, to)
+      else
+        # step to edge, wrap, step the remaining
+        is_backward = steps.negative?
+        if is_backward
+          [range.begin, range.end]
+        else
+          [range.end, range.begin]
+        end => from_edge, to_edge
+        stop = step(from, from_edge)
+        if stop != from_edge
+          stop
+        elsif (is_backward ? obstructions.max : obstructions.min) == to_edge
+          from_edge
+        else
+          step(to_edge, (to - range.begin) % range.size + range.begin)
+        end
+      end
+    end
+  end
+  
+  attr_reader :rows, :columns
+  attr_accessor :x, :y, :facing
+  def initialize(
+    rows: [], columns: [],
+    x: rows.fetch(0).range.begin, y: 0, facing: 0
+  )
+    @rows, @columns, @x, @y, @facing = rows, columns, x, y, facing
+  end
+  
+  def self.table_to_sequences(table, is_land: :itself, is_obstruction:)
+    table.map { LandSequence[*_1, is_land:, is_obstruction:] }
+  end
+  def self.[](table, is_land: :itself, is_obstruction:)
+    ConvexBoard.new(
+      rows: table_to_sequences(table, is_land:, is_obstruction:),
+      columns: table_to_sequences(table.transpose, is_land:, is_obstruction:)
+    )
+  end
+  
+  def row = y.succ
+  def column = x.succ
+  def facing! = self.facing %= 4
+  def password = 1000 * row + 4 * column + facing!
+  
+  def move(steps = 1)
+    case facing!
+      when 0 then self.x =    rows.fetch(y).step_by(x, steps)
+      when 1 then self.y = columns.fetch(x).step_by(y, steps)
+      when 2 then self.x =    rows.fetch(y).step_by(x, -steps)
+      when 3 then self.y = columns.fetch(x).step_by(y, -steps)
+      else # should not happen
+    end
+  end
+  
+  def l = self.facing -= 1
+  def r = self.facing += 1
+  def call(instruction) print(instruction)
+    case instruction
+      when 'R' then r
+      when 'L' then l
+      else move(Integer(instruction)); print "\t#{x}\t#{y}"
+    end;puts
+    self
+  end
+end
+
+*board, _, instructions = File.readlines('input.txt', chomp: true)
+board_width = board.map(&:size).max
+board = ConvexBoard[
+  board.map { _1.ljust(board_width).bytes },
+  is_land:        ->{ _1 != 0x20 }, # ` `
+  is_obstruction: ->{ _1 == 0x23 }  # `#`
+]
+
+instructions.scan(/\d++|./) { board.(_1) }
+
+puts 'Part 1', board.password
